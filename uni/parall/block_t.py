8fc8f4aaef29d135c590126df1fe99e31121094a
morena <web.iamsrn@gmail.com>
2022-12-02 10:36:13 +0400

    mpi transpose pre-alpha

--- uni/parall/block_t.py
+++ uni/parall/block_t.py
@@ -7,12 +7,12 @@ import numpy as np
 def get_send_params(dim: int, n_proc: int):
     if dim % n_proc == 0:
         count_1 = count_2 = int(dim / n_proc)
-        last_with_count_1 = n_proc - 1
+        total_with_count_1 = n_proc
     else:
         count_1 = int(dim / n_proc) + 1
         count_2 = int(dim / n_proc)
-        last_with_count_1 = dim % n_proc - 1
-    return count_1, last_with_count_1, count_2
+        total_with_count_1 = dim % n_proc
+    return count_1, total_with_count_1, count_2
 
 
 def get_test_mm_matrix(m: int):
@@ -20,50 +20,46 @@ def get_test_mm_matrix(m: int):
 
 
 def mpi_func(dim: int = None):
-    # m = m
-    # n = n
 
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    am1, last1, am2 = get_send_params(dim, size)
-    str_num = am1 if rank <= last1 else am2
+    am1, total1, am2 = get_send_params(dim, size)
+    str_num = am1 if rank < total1 else am2
     data = np.empty(str_num * dim, dtype=np.float_)
 
     if rank == 0:
         matrixOrig = get_test_mm_matrix(dim)
         data = np.copy(matrixOrig[0: am1])
-        print(matrixOrig)
+        print('--- original ---\n', matrixOrig, '\n---')
 
-        for i in range(1, last1 + 1):
+        for i in range(1, total1):
             comm.Send(np.copy(matrixOrig[i * am1: am1 + i * am1]), dest=i)
 
-        for i in range(last1 + 1, size):
-            comm.Send(np.copy(matrixOrig[(last1 + 1) * am1 + (i - last1 - 1) * am2:
-                                         (last1 + 1) * am1 + am2 + (i - last1 - 1) * am2]), dest=i)
+        for i in range(total1, size):
+            comm.Send(np.copy(matrixOrig[total1 * am1 + (i - total1) * am2:
+                                         total1 * am1 + am2 + (i - total1) * am2]), dest=i)
 
     else:
         comm.Recv(data, source=0)
 
     data = np.reshape(data, (str_num, dim))
-    print('rank: ', rank, '\ndata: ', data)
+    # print('rank: ', rank, '\ndata: ', data)
 
     obj_np_arr = np.empty(size, dtype=object)
     added = 0
     for i in range(0, size):
-        col_num = am1 if i <= last1 else am2
+        col_num = am1 if i < total1 else am2
         obj_np_arr[i] = np.copy(data[:, added: added + col_num])
         added += col_num
-
-    del data
 
     for i in range(0, size):
         elem = np.transpose(obj_np_arr[i])
         if rank == i:
             obj_np_arr[i] = elem
         else:
-            col_num = am1 if i <= last1 else am2
+            col_num = am1 if i < total1 else am2
             comm.Send(np.reshape(elem, (str_num * col_num)), dest=i)
         del elem
 
@@ -72,28 +68,27 @@ def mpi_func(dim: int = None):
             pass
         else:
             comm.Recv(obj_np_arr[i], source=i)
+    
+    added = 0
+    for i in range(0, size):
+        col_num = am1 if i < total1 else am2
+        data[:, added: added + col_num] = obj_np_arr[i]
+        added += col_num
+        
+    # print('rank: ', rank, '\ndata: ', data)
+    
+    if rank == 0:
+        matrixOrig[0: am1] = data
 
-    print('rank: ', rank, '\narr: ', obj_np_arr)
+        for i in range(1, total1):
+            comm.Recv(matrixOrig[i * am1: am1 + i * am1], source=i)
 
-    # for k in range(0, size):
-    #     for i in range(str_num):
-    #         col_num = am1 if i <= last1 else am2
-    #         for j in range(col_num):
-    #             data[i, j] = list_of_blocs[k][i][j]
-    # if rank == 0:
-    #     matrixOrig.reshape()
-    #     data = np.copy(matrixOrig[0: am1])
-    #     print(matrixOrig)
-    # 
-    #     for i in range(1, last1 + 1):
-    #         comm.Isend(np.copy(matrixOrig[i * am1: am1 + i * am1]), dest=i)
-    # 
-    #     for i in range(last1 + 1, size):
-    #         comm.Isend(np.copy(matrixOrig[(last1 + 1) * am1 + (i - last1 - 1) * am2:
-    #                                       (last1 + 1) * am1 + am2 + (i - last1 - 1) * am2]), dest=i)
-    # 
-    # else:
-    #     req = comm.Isend(obj_np_arr, source=0)
+        for i in range(total1, size):
+            comm.Recv(matrixOrig[total1 * am1 + (i - total1) * am2:
+                                 total1 * am1 + am2 + (i - total1) * am2], source=i)
+        print('--- transposed ---\n', matrixOrig, '\n---')
+    else:
+        comm.Send(data, dest=0)
 
 
 if __name__ in "__main__":
